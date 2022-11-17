4a8ec7abed087ade6ec3475ea3cf2cfee64d2a11
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-11-17 08:50:32 +0000

    Update example-functions.php

--- example-functions.php
+++ example-functions.php
@@ -1,29 +1,29 @@
 <?php
 
-function printDetails($title,$year)
+function printDetails($title, $year)
 {
 	echo "<p>The film {$title} was released in {$year}</p>";
 }
 
 function convertToEuros(int $pounds)
 {
-	return $pounds*1.18;
+	return $pounds * 1.18;
 }
 
 
-function addTwoNumbers($num1,$num2)
+function addTwoNumbers($num1, $num2)
 {
-	return $num1+$num2;
+	return $num1 + $num2;
 }
 
 
 function getWinnersByContinent(string $continent)
 {
-	$matchingCountries=[];
-	if($continent==="Europe"){
-		$matchingCountries=["France","Germany","Spain","England","Italy"];
-	}else if($continent==="South America"){
-		$matchingCountries=["Argentina","Brazil","Uruguay"];
+	$matchingCountries = [];
+	if($continent === "Europe"){
+		$matchingCountries = ["France","Germany","Spain","England","Italy"];
+	}else if($continent === "South America"){
+		$matchingCountries  =["Argentina","Brazil","Uruguay"];
 	}
 	return $matchingCountries;
 }
@@ -43,11 +43,11 @@ function getPositiveNumbers(array $arrOfNumbers)
 
 function filterImageFileNames(array $arrOfFileNames)
 {
-	$matchingFileNames=[];
+	$matchingFileNames = [];
 	foreach($arrOfFileNames as $filename){
 		$fileExtension = substr($filename, strrpos($filename, '.') + 1);
-		if($fileExtension==="png" || $fileExtension==="jpg" || $fileExtension==="jpeg"){
-			$matchingFileNames[]=$filename;
+		if($fileExtension === "png" || $fileExtension === "jpg" || $fileExtension === "jpeg"){
+			$matchingFileNames[] = $filename;
 		}
 	}
 	return $matchingFileNames;
